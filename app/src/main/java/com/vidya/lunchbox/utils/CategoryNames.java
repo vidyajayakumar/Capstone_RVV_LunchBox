a89ed4f2b15751cedff4a8c8fea240281b7b7da7
vidya <vvallive@gmail.com>
2020-08-24 05:19:42 +0530

    added list of Category names in CategoryNames class

---
+++ app/src/main/java/com/vidya/lunchbox/utils/CategoryNames.java
@@ -0,0 +1,10 @@
+package com.vidya.lunchbox.utils;
+
+public class CategoryNames {
+
+    public static final String MAINCOURSE_CATEGORY = "MAIN COURSE";
+    public static final String DESERTS_CATEGORY = "DESERTS";
+    public static final String BEVARAGES_CATEGORY = "BEVERAGES";
+    public static final String APETIZERS_CATEGORY = "APPETIZERS";
+
+}
