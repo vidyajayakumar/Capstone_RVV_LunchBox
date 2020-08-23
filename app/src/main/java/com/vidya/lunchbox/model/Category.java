ae87d343a10f3a2075d2463d1d5d20b296fd49d2
vidya <vvallive@gmail.com>
2020-08-24 05:06:41 +0530

    created model class for Category

---
+++ app/src/main/java/com/vidya/lunchbox/model/Category.java
@@ -0,0 +1,22 @@
+package com.vidya.lunchbox.model;
+
+public class Category {
+    private String description;
+    private int imgId;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+}
