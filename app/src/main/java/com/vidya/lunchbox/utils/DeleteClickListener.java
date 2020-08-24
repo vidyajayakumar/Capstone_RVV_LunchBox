054e1a919b47790ec87a00ed09936280483281f7
vidya <vvallive@gmail.com>
2020-08-24 07:06:32 +0530

    added interfaces for delete and add click listerners in Cart and Category screens

---
+++ app/src/main/java/com/vidya/lunchbox/utils/DeleteClickListener.java
@@ -0,0 +1,7 @@
+package com.vidya.lunchbox.utils;
+
+import com.vidya.lunchbox.model.Cart;
+
+public interface DeleteClickListener {
+    void onClick(Cart item);
+}
---
+++ app/src/main/java/com/vidya/lunchbox/utils/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.vidya.lunchbox.utils;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, String category);
+}
