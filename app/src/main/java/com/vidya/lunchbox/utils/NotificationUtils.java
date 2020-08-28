cd8726c07e296aa992cad59c3ef31f8871753841
vidya <vvallive@gmail.com>
2020-08-28 23:23:24 +0530

    Update NotificationUtils.java

--- app/src/main/java/com/vidya/lunchbox/utils/NotificationUtils.java
+++ app/src/main/java/com/vidya/lunchbox/utils/NotificationUtils.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.graphics.Color;
@@ -14,9 +15,9 @@ import androidx.annotation.RequiresApi;
 
 public class NotificationUtils extends ContextWrapper {
 
-    private NotificationManager mManager;
     public static final String ANDROID_CHANNEL_ID = "com.example.lunchbox.ANDROID";
     public static final String ANDROID_CHANNEL_NAME = "ANDROID CHANNEL";
+    private NotificationManager mManager;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public NotificationUtils(Context base) {
@@ -50,11 +51,12 @@ public class NotificationUtils extends ContextWrapper {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public Notification.Builder getAndroidChannelNotification(String title, String body) {
+    public Notification.Builder getAndroidChannelNotification(String title, String body, PendingIntent pendingIntent) {
         return new Notification.Builder(getApplicationContext(), ANDROID_CHANNEL_ID)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setSmallIcon(android.R.drawable.stat_notify_more)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
     }
 
